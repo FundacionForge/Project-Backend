69a921b1e99093653e4dfb7d206e6556d382f3c2
jeff <kloudqp@gmail.com>
2023-07-26 12:05:41 -0500

    Aislando el RepositoryBase para ahorrar codigo.

--- src/main/java/com/example/forge/repositories/UserRepository.java
+++ src/main/java/com/example/forge/repositories/UserRepository.java
@@ -2,10 +2,10 @@ package com.example.forge.repositories;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import com.example.forge.models.entities.User;
-import com.example.forge.repositorybase.RepositoryBase;
 
-public interface UserRepository extends RepositoryBase<User>{
+public interface UserRepository extends CrudRepository<User, Long>{
   Optional<User> findByUsername(String username);
 
   @Query("select u from User u where u.username=?1")
