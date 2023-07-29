2ab93ad8e507c27ee66054f8da77effa20711a2d
Marcos Alanya <asesoralanya19@gmail.com>
2023-07-28 21:00:09 -0500

    ADD: Implementacion de repository pagination

--- src/main/java/com/example/forge/repositories/BaseRepository.java
+++ src/main/java/com/example/forge/repositories/BaseRepository.java
@@ -2,6 +2,8 @@ package com.example.forge.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 @NoRepositoryBean
-public interface BaseRepository<T> extends CrudRepository<T, Long> {}
+public interface BaseRepository<T> extends CrudRepository<T, Long>,
+PagingAndSortingRepository<T, Long> {}
