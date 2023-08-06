d272f5e4fb9198dd87093688ba850b7032e55f08
Marcos Alanya <asesoralanya19@gmail.com>
2023-08-06 15:19:27 -0500

    CONFIG: Dockerfile9

--- src/main/java/com/example/forge/services/UserService.java
+++ src/main/java/com/example/forge/services/UserService.java
@@ -1,16 +1,80 @@
 package com.example.forge.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.forge.models.dto.UserDto;
+import com.example.forge.models.dto.mapper.DtoMapperUser;
+import com.example.forge.models.entities.Role;
 import com.example.forge.models.entities.User;
 import com.example.forge.models.request.UserRequest;
+import com.example.forge.repositories.RoleRepository;
+import com.example.forge.repositories.UserRepository;
 
-public interface UserService {
-  List<UserDto> findAll();
-  Optional<UserDto> findById(Long id);
-  UserDto save(User user);
-  Optional<UserDto> update(UserRequest user, Long id);
-  void remove(Long id);
+@Service
+public class UserService {
+  @Autowired
+  private UserRepository repository;
+
+  @Autowired
+  private RoleRepository roleRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  @Transactional(readOnly = true)
+  public List<UserDto> findAll() {
+    List<User> users = (List<User>) repository.findAll();
+    return users
+      .stream()
+      .map(u -> DtoMapperUser.builder().setUser(u).build())
+      .collect(Collectors.toList());
+  }
+
+  @Transactional(readOnly = true)
+  public Optional<UserDto> findById(Long id) {
+    return repository.findById(id).map(u -> DtoMapperUser
+    .builder()
+    .setUser(u)
+    .build());
+  }
+
+  @Transactional
+  public UserDto save(User user) {
+    String passwordBCrypt =  passwordEncoder.encode(user.getPassword());
+    user.setPassword(passwordBCrypt);
+
+    Optional<Role> o = roleRepository.findByName("ROLE_USER");
+    List<Role> roles = new ArrayList<>();
+    if(o.isPresent()) {
+      roles.add(o.orElseThrow());
+    }
+    user.setRoles(roles);
+    return DtoMapperUser.builder().setUser(repository.save(user)).build();
+  }
+
+  @Transactional
+  public Optional<UserDto> update(UserRequest user, Long id) {
+    Optional<User> o = repository.findById(id);
+    User userOptional = null;
+      if(o.isPresent()){
+        User userDb = o.orElseThrow();
+        userDb.setUsername(user.getUsername());
+        userDb.setEmail(user.getEmail());
+        userOptional = repository.save(userDb);
+    }
+    return Optional.ofNullable(DtoMapperUser.builder().setUser(userOptional).build());
+  }
+
+  @Transactional
+  public void remove(Long id) {
+    repository.deleteById(id);
+  }
 }
